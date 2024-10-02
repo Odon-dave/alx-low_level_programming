33463c55e7863ac3b07322dde7ac4a08353fad28
Odon-dave <odondave9@gmail.com>
2024-10-02 05:43:17 -0700

    task 5

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -program that print memory size of
+ * various data types on a computer using printf
+ * Return:0 if the program is success
+ */
+int main(void)
+{
+int UniqueIntVar;
+float UniqueFloatVar;
+double UniqueDoubleVar;
+char UniqueCharVar;
+
+	printf("%lu\n", size of(UniqueIntVar));
+	printf("%lu\n", size of(UniqueFloatVar));
+	printf("%lu\n", size of(UniqueDoubleVar));
+	printf("%lu\n", size of(UniqueCharVar));
+	return (0);
+}
