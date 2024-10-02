8f2545ec835544e67b54ab33fa54bec90bad66c3
Odon-dave <odondave9@gmail.com>
2024-10-02 03:50:49 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - program that prints a line with print f function
+ *Return: 0 if the program runs succesfully
+ */
+	int main(void)
+{
+	printf("with proper grammar\, but the outcome is a piece of art\,\n");
+	return (0);
+}
