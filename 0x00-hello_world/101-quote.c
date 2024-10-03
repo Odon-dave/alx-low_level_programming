b5ca749deb819ecc9db361b98c27aa8f5818ebc6
Odon-dave <odondave9@gmail.com>
2024-10-03 01:35:51 -0700

    task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- program that prints a line to standard error
+ * Return:1 if the program is a success
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19, stdout");
+	return (1);
+}
