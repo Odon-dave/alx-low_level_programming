c265d4dc682b0d714e1221fe144098eb2b1350a0
Odon-dave <odondave9@gmail.com>
2025-03-12 16:33:19 +0300

    new task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - a program that prints exactly the sizes of various types on the computer
+ * main - a program that prints exactly
+ *  the sizes of various types on the computer
  * Return: 0 (success)
  */
 int main(void)
