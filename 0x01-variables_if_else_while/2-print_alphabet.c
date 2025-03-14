40d21439c61914a4ac152e2334e708fcf1d5dccc
Odon-dave <odondave9@gmail.com>
2025-03-14 18:38:53 +0300

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a program that prints alphabetical letters
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char letter;
+for (letter ='a';letter<='z';letter++)
+putchar(letter);
+putchar('\n');
+return(0);
+}
