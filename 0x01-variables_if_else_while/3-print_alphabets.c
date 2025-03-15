0e6dadfd4e4aaa05e42b44a610dc1419a9f47e2d
Odon-dave <odondave9@gmail.com>
2025-03-16 01:21:25 +0300

    03 task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,13 +3,13 @@
  * main - a program that prints alphabeticalletters in both upper and lowe case
  * Return: 0 (success)
  */
-int main()
+int main(void)
 {
 char letter;
 char alphabet;
-for (letter = 'a' ; letter <= 'z' ; letter ++)
+for (letter = 'a' ; letter <= 'z' ;letter ++)
 putchar (letter);
-for (alphabet = 'A' ; alphabet <= 'Z' ; alphabet ++)
+for (alphabet = 'A' ; alphabet <= 'Z' ;alphabet ++)
 putchar (alphabet);
 putchar ('\n');
 return (0);
