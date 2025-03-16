251d25766fcfb4c99f502e3d3131a37a74a3640c
Odon-dave <odondave9@gmail.com>
2025-03-16 03:01:18 +0300

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - a program that prints numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i=0; i <=9; i++)
+{
+printf ("%d",i);
+}
+printf ("\n");
+return (0);
+}
