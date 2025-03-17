3536e6fbf501009f9fc493b336d5086ff84e7813
Odon-dave <odondave9@gmail.com>
2025-03-17 21:03:56 +0300

    new task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-int a,b,c;
+int a, b, c;
 for (a = 0; a <= 7; a++)
 {
 for (b = a + 1; b <= 8; b++)
@@ -15,7 +15,7 @@ for (c = b + 1; c <= 9; c++)
 putchar(a + '0');
 putchar(b + '0');
 putchar(c + '0');
-if (a != 7 || b != 8 || c!= 9)
+if (a != 7 || b != 8 || c != 9)
 {
 putchar(',');
 putchar(' ');
