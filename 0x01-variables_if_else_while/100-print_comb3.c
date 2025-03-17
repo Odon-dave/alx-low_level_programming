ea39af31cc7cfce94809786dde58c9a7f8bda0fa
Odon-dave <odondave9@gmail.com>
2025-03-17 20:17:23 +0300

    10 task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-int a,b;
+int a , b;
 for (a = 0; a <= 8; a++)
 {
-for (b = a +1; b <= 9; b++)
+for (b = a + 1; b <= 9; b++)
 {
 {
 putchar(a + '0');
 putchar(b + '0');
 }
-if (a !=8 || b !=9 )
+if (a != 8 || b != 9 )
 {
 putchar(',');
 putchar(' ');
