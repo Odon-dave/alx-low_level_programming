039fd276368638329c5a507891649d2542052638
Odon-dave <odondave9@gmail.com>
2025-03-18 03:21:00 +0300

    02 task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,12 @@ void print_alphabet_x10(void)
 {
 int i;
 char letter;
-for (i = 0; i <= 9, i++)
+for (i = 1; i <= 10, i++)
 {
-    _putchar(i + '0');
-    for (char = 'a', char <= 'z', char ++)
-    {
-        _putchar(char);
-    }
-    _putchar('\n');
+for (char = 'a', char <= 'z', char ++)
+{
+_putchar(char);
+}
+_putchar('\n');
 }
 }
