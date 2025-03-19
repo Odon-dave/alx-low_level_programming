7eb9f079c433af7821eecdfd0639944be7758269
Odon-dave <odondave9@gmail.com>
2025-03-20 00:40:19 +0300

    new task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit- a program that prints last digit
+ * Return:last_digit (success)
+ */
+int print_last_digit(int)
+last_digit = n % 10;
+{
+if (last_digit > 0)
+{
+_putchar(last_digit);
+}
+else
+{
+return (last_digit);
+}
+}
