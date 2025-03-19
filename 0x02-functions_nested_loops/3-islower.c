f414932a919b524df34d76976ded60e3b352920f
Odon-dave <odondave9@gmail.com>
2025-03-19 18:54:54 +0300

    checking lower character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int _islower- program that checks for lower character
+ *_islower- program that checks for lower character
  * @c: character to be checked
  * Return: 1 if c is lowercase, 0 otherwise
  */
