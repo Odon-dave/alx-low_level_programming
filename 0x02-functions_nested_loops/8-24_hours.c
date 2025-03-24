9b35a00362af256786cbebc6165e9d65912e7bb3
Odon-dave <odondave9@gmail.com>
2025-03-24 20:53:26 +0300

    printing hours of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,12 +11,10 @@ for (minute = 0; minute < 60; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
-_putchar(';');
-_putchar('\n');
-}
+_putchar(':');
 _putchar((minute / 10) + '0');
 _putchar((minute % 10) + '0');
-_putchar(';');
+_putchar(':');
 _putchar('\n');
 }
 }
