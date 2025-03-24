03e2ce0b3f283f714486ee9949fe33d549bdda7d
Odon-dave <odondave9@gmail.com>
2025-03-24 20:40:26 +0300

    printing times of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * jack_bauer- function that prints every minute of the day
- * Return: 0 (success)
  */
 void jack_bauer(void)
 {
@@ -20,5 +19,4 @@ _putchar((minute % 10) + '0');
 _putchar(';');
 _putchar('\n');
 }
-return (0);
 }
