fe689ac4963db5c9fb96d8f47c58cd59d98adada
Odon-dave <odondave9@gmail.com>
2025-04-12 12:47:16 +0300

    11th task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_number- a helper to print an integer 
+ * print_number- a helper to print an integer
  */
 void print_number(int n)
 {
@@ -10,11 +10,11 @@ if (n < 0)
 putchar('-');
 n = -n;
 }
-if (n /10)
+if (n / 10)
 {
-print_number(n /10);
+print_number(n / 10);
 }
-putchar((n %10) +'0');
+putchar((n % 10) +'0');
 }
 /**
 * print_to_98- function that prints natural numbers to 98
