cf8a65200b5e01dab05d20cdcfe9e5fd15f7e284
Odon-dave <odondave9@gmail.com>
2025-04-12 12:20:31 +0300

    011 task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,21 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * print_number- a helper to print an integer 
+ */
+void print_number(n)
+{
+if (n < 0)
+{
+putchar('-');
+n = -n;
+}
+if (n /10)
+{
+print_number(n /10);
+putchar((n %10) +'0');
+}
+}
 /**
 * print_to_98- function that prints natural numbers to 98
 * @n: number to be checked
@@ -11,7 +28,7 @@ for (; n <= 98; n++)
 {
 print_number(n);
 if (n != 98)
-putchar(",");
+putchar(',');
 putchar(' ');
 }
 }
