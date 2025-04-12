7e26cc7de667d2acbf07d079f86cc34bc871878a
Odon-dave <odondave9@gmail.com>
2025-04-12 11:35:19 +0300

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* print_to_98- function that prints natural numbers to 98
+* @n: number to be checked
+*/
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+print_number(n);
+if (n != 98)
+_putchar(",");
+_putchar(' ');
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+print_number(n);
+if (n != 98)
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
