dce320ec8cee4411f707fed2be734b40b9d6abe0
Odon-dave <odondave9@gmail.com>
2025-06-09 16:21:02 +0300

    task 0

---
+++ 0x04-more_functions_nested_loop/0-isupper.c
@@ -0,0 +1,18 @@
+#include <main..h>
+
+/**
+ * int _isupper : function that checks  for upper case
+ * return : 1 (success)
+ */
+
+ int _isupper(int c)
+ {
+    if (c >= A && c <= Z)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+ }
