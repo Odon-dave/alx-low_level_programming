4dbaba77c0bbd82090f9ed340b2e1b38e48dbda1
Odon-dave <odondave9@gmail.com>
2025-06-23 14:02:14 +0300

    001

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * swap_int - function to swap integers
+ * @a:integer to be swapped
+ * @b:integer to be swapped
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a
+*a = *b
+*b = temp
+}
